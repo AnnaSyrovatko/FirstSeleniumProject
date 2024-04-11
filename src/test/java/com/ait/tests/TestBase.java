bbe893e1008baf48591801e0c0562910d4dc55f2
Anna Syrovatko <annasyrovatko76@gmail.com>
2024-04-11 12:57:44 +0700

    logs add

--- src/test/java/com/ait/tests/TestBase.java
+++ src/test/java/com/ait/tests/TestBase.java
@@ -2,14 +2,21 @@ package com.ait.tests;
 
 import com.demoshop.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp(){
@@ -19,6 +26,21 @@ public class TestBase {
     @AfterSuite //(enabled = false)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start test" + method.getName());
+    }
+    @AfterMethod
+    public void stopTest(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("PASSED " + result.getMethod().getMethodName());
+        } else {
+            logger.error("FAILED " + result.getMethod().getMethodName() + "Screenshot: " + app.getUser());
+        }
+        logger.info("Stop test");
+        logger.info("=============================================");
     }
 
 }
